72ce2fe44e3896c7531779e317373671c3b38ee2
kuaidev <dominguezluciano336@protonmail.com>
2022-07-24 00:39:18 +0200

    Changed sendFile to render for views

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.set('view engine','ejs')
 app.listen(3000);
 
 app.get('/', (req,res)=>{
-    res.sendFile('./views/index.html',{ root: __dirname});
+   res.render('index');
 
 })
 
 app.get('/about', (req,res)=>{
-    res.sendFile('./views/about.html',{ root: __dirname});
+    res.render('about');
 
 })
 
